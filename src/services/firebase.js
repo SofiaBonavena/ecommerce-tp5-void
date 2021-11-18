ced1c76a1dd5d263cf707dae3fa731cf05bcd87e
Sofi <sofi-b15@hotmail.com.ar>
2021-11-17 23:54:14 -0300

    segundocommit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import '@firebase/firestore';
 
 // Your web app's Firebase configuration
 const app = {
-  apiKey: "AIzaSyDYk6VnXbZLJSyG6JVP0EfcxUyNjfBA-30",
-  authDomain: "maimo-commerce-sofi.firebaseapp.com",
-  projectId: "maimo-commerce-sofi",
-  storageBucket: "maimo-commerce-sofi.appspot.com",
-  messagingSenderId: "632670397091",
-  appId: "1:632670397091:web:5d6749a25006a7d65b5516"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 if (!firebase.apps.length) {
